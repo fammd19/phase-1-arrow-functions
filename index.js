3198b8af242ba71195d9b9595ee7e5d10b412018
Fiona Mellink <fimellink@gmail.com>
2023-12-16 12:56:51 +1100

    Complete lab

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide (a, b) {
+    return a/b; 
+}
+
+let square = (x) => x*x;
+
+let add = (a, b) => a+b;
+
+console.log(square(2));
+console.log(divide(20000,100));
+console.log(add(3,4));
